963f96d565fa3b8268f7ad989f24469cbfec0572
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-27 11:14:03 +0300

    Blogform näkyy vasta painiketta painettaessa

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -4,6 +4,7 @@ export const BlogForm = ({ addBlog }) => {
   const [newTitle, setNewTitle] = useState("");
   const [newAuthor, setNewAuthor] = useState("");
   const [newUrl, setNewUrl] = useState("");
+  const [formVisible, setFormVisible] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -12,41 +13,60 @@ export const BlogForm = ({ addBlog }) => {
       setNewTitle("");
       setNewAuthor("");
       setNewUrl("");
+      setFormVisible(false);
     }
   };
 
-  return (
-    <>
-      <h2>Add new blog</h2>
+  const handleCancel = (event) => {
+    event.preventDefault();
+    setFormVisible(false);
+  };
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          title:{" "}
-          <input
-            value={newTitle}
-            onChange={(event) => setNewTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          author:{" "}
-          <input
-            value={newAuthor}
-            onChange={(event) => setNewAuthor(event.target.value)}
-          />
-        </div>
-        <div>
-          url:{" "}
-          <input
-            value={newUrl}
-            onChange={(event) => setNewUrl(event.target.value)}
-          />
-        </div>
-        <div>
-          <button name="add" aria-labelledby="add blog" type="submit">
-            add
-          </button>
-        </div>
-      </form>
-    </>
-  );
+  if (formVisible) {
+    return (
+      <>
+        <h2>Add new blog</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            title:{" "}
+            <input
+              value={newTitle}
+              onChange={(event) => setNewTitle(event.target.value)}
+            />
+          </div>
+          <div>
+            author:{" "}
+            <input
+              value={newAuthor}
+              onChange={(event) => setNewAuthor(event.target.value)}
+            />
+          </div>
+          <div>
+            url:{" "}
+            <input
+              value={newUrl}
+              onChange={(event) => setNewUrl(event.target.value)}
+            />
+          </div>
+          <div>
+            <button name="save" aria-labelledby="save new blog" type="submit">
+              save
+            </button>
+            <button onClick={handleCancel}>cancel</button>
+          </div>
+        </form>
+      </>
+    );
+  } else {
+    return (
+      <button
+        onClick={() => setFormVisible(true)}
+        name="add new blog"
+        aria-labelledby="add blog"
+      >
+        add new blog
+      </button>
+    );
+  }
 };
